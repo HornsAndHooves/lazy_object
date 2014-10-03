e38a4676848e4778d5b95357b00a2a71758e2127
Arthur Shagall <arthur.shagall@gmail.com>
2014-10-02 20:16:34 -0500

    Add specs.

---
+++ spec/lazy_object_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.describe LazyObject do
+
+  class TargetObject
+    attr_accessor :value
+    
+    def method_with_yield(foo)
+      yield foo
+    end
+  end
+  
+  let(:lazy_object) {
+    LazyObject.new do
+      targ = TargetObject.new
+      targ.value = :foo
+      targ
+    end
+  }
+  
+  it 'should call the init block and forward methods to the target object' do
+    expect( lazy_object.value ).to eq(:foo)
+    lazy_object.value = :bar
+    expect( lazy_object.value ).to eq(:bar)
+    
+    expect( lazy_object.method_with_yield(:baz) { |foo| "--#{foo}--" } ).to eq('--baz--')
+  end
+
+end
