2f41d999029357bbe4d4647f4ade33c8bcf5cc5b
Sapozhnikov, Andrey <skarm61@gmail.com>
2015-03-04 09:58:42 +0200

    Fix according commits

--- lib/lazy_object.rb
+++ lib/lazy_object.rb
@@ -8,10 +8,10 @@
 #
 # lazy = LazyObject.new { VeryExpensiveObject.new } # At this point the VeryExpensiveObject hasn't been initialized yet.
 # lazy.get_expensive_results(foo, bar) # Initializes VeryExpensiveObject and calls 'get_expensive_results' on it, passing in foo and bar
-class LazyObject
+class LazyObject < BasicObject
 
   def self.version
-    '0.0.2'
+    '0.0.1'
   end
 
   def initialize(&callable)
